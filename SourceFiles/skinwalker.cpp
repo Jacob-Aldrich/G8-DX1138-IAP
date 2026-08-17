21b3d72bedf1cd68dd309e6af648290bb8efdf47
gaowenya110-svg <gaowenya110@gmail.com>
2026-08-17 21:11:27 +0800

    Create skinwalker.cpp

---
+++ SourceFiles/skinwalker.cpp
@@ -0,0 +1,21 @@
+#include "skinwalker.h"
+#include <iostream>
+void skinwalker::dialogue()
+{
+	int randomiser = rand() % 3;
+	setDialogue(randomiser);
+
+	if (randomiser == 0)
+	{
+		std::cout << "PleAse hElp mE, I neeD sheltEr." << std::endl;
+	}
+	else if (randomiser == 1)
+	{
+		std::cout << "Hey, could you let me in? I've got some suplies to share." << std::endl;
+	}
+	else
+	{
+		std::cout << "...Young one... Let me in." << std::endl;
+	}
+}
+
