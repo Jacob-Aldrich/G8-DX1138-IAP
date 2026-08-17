51039596224b9a9a4ddd9c36446980866bf57bff
Jacob-Aldrich <jake.aldree@gmail.com>
2026-08-17 16:39:05 +0800

    Add files via upload

---
+++ SourceFiles/Entity.cpp
@@ -0,0 +1,39 @@
+#include "Entity.h"
+
+Entity::Entity(int startingHealthPoints, int startingAttackPoints)
+{
+    HealthPoints = startingHealthPoints;
+    AttackPoints = startingAttackPoints;
+}
+
+int Entity::GetHealthPoints()
+{
+    return HealthPoints;
+}
+
+int Entity::GetBaseAttackPoints()
+{
+    return AttackPoints;
+}
+
+bool Entity::IsAlive()
+{
+    return HealthPoints > 0;
+}
+
+void Entity::TakeDamage(int DamagePoints)
+{
+    if (DamagePoints >= HealthPoints)
+    {
+        HealthPoints = 0;
+    }
+    else
+    {
+        HealthPoints -= DamagePoints;
+    }
+}
+
+int Entity::Attack()
+{
+    return attackPoints;
+}
---
+++ SourceFiles/Object.cpp
@@ -0,0 +1,12 @@
+#include "Object.h"
+
+// Default constructor
+Object::Object()
+{
+}
+
+// Virtual destructor so derived objects can be safely deleted
+// through an Object pointer
+Object::~Object()
+{
+}
