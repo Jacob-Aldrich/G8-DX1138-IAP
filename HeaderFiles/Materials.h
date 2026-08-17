22cee5e659c622c4551665c2250d29175b8808a3
BrandonChai-91 <brandonchai91@gmail.com>
2026-08-17 16:50:02 +0800

    Add files via upload

---
+++ HeaderFiles/Materials.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Object.h"
+
+class Material : public Object
+{
+private:
+
+    int quantity;
+    int maximumQuantity;
+
+public:
+
+    Material(int startingQuantity, int startingMaximumQuantity);
+
+    int GetQuantity();
+    int GetMaximumQuantity();
+
+    bool IsEmpty();
+
+    void AddQuantity(int quantityToAdd);
+
+    bool UseQuantity(int quantityToUse);
+};
