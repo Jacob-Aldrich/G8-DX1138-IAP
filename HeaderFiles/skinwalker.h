4d414b8b641f205aef7d612d2a2a70e1eb7daf03
gaowenya110-svg <gaowenya110@gmail.com>
2026-08-17 21:12:52 +0800

    Create skinwalker.h

---
+++ HeaderFiles/skinwalker.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Entity.h"
+
+class skinwalker : public Entity
+{
+private:
+	int SWHealthPoints;
+	int SWAttackPoints;
+
+public:
+	virtual void dialogue();
+	void setDialogue(int randomiser);
+	skinwalker image();
+	int GetSWHealthPoints();
+	int GetSWAttackPoints();
+
+	bool IsLetIn();
+};
