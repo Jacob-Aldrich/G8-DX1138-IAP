c0c631c0ee3c0607d12709d4a4e862de927ea1d9
shan <shan.lyxs9@gmail.com>
2026-08-18 10:40:51 +0800

    Player.h
    1st code

--- HeaderFiles/Player.h
+++ HeaderFiles/Player.h
@@ -1,1 +1,34 @@
+#pragma once
+#include"Entity.h"
+#include <string>
+
+class Player : public Entity
+{
+private:
+    std::string Look;
+
+    std::string Dialogue1;
+    std::string Dialogue2;
+	std::string Dialogue3;
+
+public:
+	Player();
+
+	void Speak(int choice);
+
+	void SetLook(const std::string& look);
+	std::string GetLook();
+
+	bool SkinwalkerAtDoor();
+	bool EncounterSkinwalker();
+
+	//actions
+	void SkinWalkerAtDoor(int choice);
+	void EncounterSkinwalker(int choice);
+
+
+
+
+};
+
 
