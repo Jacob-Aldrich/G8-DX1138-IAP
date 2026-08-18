b83a939e8222a4bf87af1a481c899e20b2ebb15f
gaowenya110-svg <gaowenya110@gmail.com>
2026-08-18 13:57:37 +0800

    Update Survivors.h

--- HeaderFiles/Survivors.h
+++ HeaderFiles/Survivors.h
@@ -1,1 +1,30 @@
+#pragma once
+#include "Entity.h"
+class Survivors :
+    public Entity
+{
+private:
+    int SHealthPoints;
+    int SAttackPoints;
+    int imagechoice;
+
+public:
+	Survivors();
+
+	virtual void dialogue();
+	void setDialogue(int randomiser);
+
+	int GetSHealthPoints(int SHealthPoints);
+	int GetSAttackPoints(int SAttackPoints);
+
+	Survivors image();
+
+	bool IsLetIn();
+
+	void choiceDialogue() const;
+	void battle() const;
+	void displayImage() const;
+
+};
+
 
