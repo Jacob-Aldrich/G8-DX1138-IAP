ea8d179ba13bdd89327446006a77d865a6eff7c4
Jacob-Aldrich <jake.aldree@gmail.com>
2026-08-18 12:12:53 +0800

    Update World.cpp

--- SourceFiles/World.cpp
+++ SourceFiles/World.cpp
@@ -5,7 +5,22 @@ World::World()
 	std::cout << "Constructing World\n";
 	Days = 0;
 }
+void World::SearchForSupplies() {
 
+	int randomFood = rand() % 3;
+	int randomWater = rand() % 3;
+	
+	Food.AddQuantity(randomFood);
+	Water.AddQuantity(randomWater);
+
+	int FoodAmount = Food.GetQuantity();
+	int WaterAmount = Water.GetQuantity();
+	int MaxFoodAmount = Food.GetMaximumQuantity();
+	int MaxWaterAmount = Water.GetMaximumQuantity();
+	
+	std::cout << "Food: " << FoodAmount << "/" << MaxFoodAmount << "\n";
+	std::cout << "Water: " << WaterAmount << "/" << MaxWaterAmount << "\n";
+}
 World::~World()
 {
 	std::cout << "Destructing World\n";
