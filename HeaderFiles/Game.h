c2f988748f75b01052503e8ecc6750f6343f807f
Jacob-Aldrich <jake.aldree@gmail.com>
2026-08-18 12:10:59 +0800

    Update Game.h

--- HeaderFiles/Game.h
+++ HeaderFiles/Game.h
@@ -6,12 +6,34 @@ class Game
 	World* world;
 	int Turns;
 	static int const maxTurns = 5;
+	int SafePlayerCount = 0;
+
 public:
 	Game();
 	void Run();
 
 	int getTurns();
 	void useTurn();
+
+	void LookForSurvivors();
+    bool UsedNames[10] = { false };
+    std::string SafePlayerNames[10] =
+    {
+        "James",
+        "Ethan",
+        "Marcus",
+        "Daniel",
+        "Ryan",
+        "Lucas",
+        "Adrian",
+        "Noah",
+        "Jason",
+        "Kevin"
+    };
+
+    Entity* SafePlayers[10] = { nullptr };
+
+	void CheckIfDead(Entity*& entity);
 
 	static void clearConsole();
 	~Game();
