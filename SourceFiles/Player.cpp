e527fa1a4ca658c049ef543b05c0749a9b9db395
shan <shan.lyxs9@gmail.com>
2026-08-18 11:37:01 +0800

    Add multiple player looks and getter methods

--- SourceFiles/Player.cpp
+++ SourceFiles/Player.cpp
@@ -4,22 +4,96 @@
 
 Player::Player() : Entity(100, 20) // Initialize base class with health and attack points
 {
-	Look = R"(                                                      
-              -  *****-                        
-            *:           %*                    
-         .+                -#                  
-        :       .------                        
-         =     .:            .-+               
-        + -.: =              .- =              
-        : -.=-.     :     :: .- +              
-        - :+- .=             .-.-              
-        +                                      
-        +      =:            +                 
-        +      %    ++++++++ .-                
-        +      -:            #                 
-        =                                      
-               
+	Look0 = R"(                                                      
+          _   ____  _.-"-._
+        .' `;'    ':.      '-.
+       /  ./     .'`\`-...-'`
+      /  /|D_  .O    |
+     /  / |=\/`=     |
+    |_.'  |  |       |
+           \  \   _ /_
+           /`---'}_()_{
+          /`'---' //\\\
+         /;      (/\ \)\
+        / |         \   \
+       /  |         |   |
+      /  / .     _ /   /
+      \_|   '. .'  '-'|
+        \    .-|     /
+     _.-'   /  |    /
+    (      /  / .--;
+     '-.__/   |    /
+              \__.
 )"; // Default look for the player
+
+	Look1 = R"(
+			  .-.
+			 (o.o)
+			  |=|
+			 __|__
+		   //.=|=.\\
+		  // .=|=. \\
+		  \\ .=|=. //
+		   \\(_=_)//
+			(:| |:)
+			 || ||
+			 () ()
+			 || ||
+			 || ||
+			==' '==
+)";
+	Look2 = R"(
+   ,^~~~-.         .-~~~"-.
+  :  .--. \       /  .--.  \
+  : (    .-`<^~~~-: :    )  :
+  `. `-,~            ^- '  .'
+    `-:                ,.-~
+      .'                  `.
+     ,'   @   @            |
+     :    __               ;
+  ...{   (__)          ,----.
+ /   `.              ,' ,--. `.
+|      `.,___   ,      :    : :
+|     .'    ~~~~       \    / :
+ \.. /               `. `--' .'
+    |                  ~----~
+    |                      |
+     ----------------------
+)";
+	Look3 = R"(
+         .--.. .--..                         
+        / #  \/ # \ \                    
+       |# # #|# # #| \                  
+      ,:  ___; ___ |  |           
+    ,~   /   \/   \    ~`.           
+   .,--.|    |     | ,.. #`.           
+   :    |  * | *   |'   `. #:        
+  |`.    \___/\___/      ;  :          
+  :  ~|     ( )       /~    #           
+  `#   \    /~\      /     |'            
+   `.   `--'   `----'    #/               
+     \#                  .'               
+      `-..             #/.                  
+      /#  ~          ~~  #\
+)";
+	Look4 = R"(
+          ..  .-~~~-. ..                  
+     .--.'  `'       '  ;.                   
+   .'                     `,                  
+   `. ,.'`.__'~-.           :               
+     ~ /~  / ~\  `-       .,'
+      |___|____| :          `.
+      |  *| *  |  `-.        ;            
+       \__/\__/      ~;     |'         
+      /  `.           `-...;'         
+      |   |                ;       
+      |   ;                |             
+      `.  `._              |         
+        \   ~~~           .'              
+         `.__________.'   :              
+                `
+)";
+
 	Dialogue1 = "\"It's been a while\"";
 	Dialogue2 = "\"Who are you?\"";
 	Dialogue3 = "\"No!\"";
@@ -43,12 +117,28 @@ void Player::Speak(int choice)
 	}
 }
 
-void Player::SetLook(const std::string& look) {
-	Look = look;
+std::string Player::GetLook0() {
+	return Look0;
 }
 
-std::string Player::GetLook() {
-	return Look;
+std::string Player::GetLook1()
+{
+	return Look1;
+}
+
+std::string Player::GetLook2()
+{
+	return Look2;
+}
+
+std::string Player::GetLook3()
+{
+	return Look3;
+}
+
+std::string Player::GetLook4()
+{
+	return Look4;
 }
 
 void Player::SkinWalkerAtDoor(int choice)
@@ -99,3 +189,53 @@ bool Player::EncounterSkinwalker() {
 	bool EncounteredSkinwalker = false;
 	return EncounteredSkinwalker;
 }
+
+
+void Player::SurvivorsAtDoor(int choice)
+{
+
+	bool survivorsAtDoor = false;
+
+	if (survivorsAtDoor) {
+		switch (choice) {
+		case 1:
+			std::cout << "Let them in" << std::endl;
+			break;
+		case 2:
+			std::cout << "Don't let them in" << std::endl;
+			break;
+		default:
+			std::cout << "..." << std::endl;
+			break;
+		}
+	}
+}
+
+//void Player::EncounterSurvivors(int choice)
+//{
+//	bool encounteredsurvivors = false;
+//
+//	if (encounteredsurvivors) {
+//		switch (choice) {
+//		case 1:
+//			std::cout << "Battle" << std::endl;
+//			break;
+//		default:
+//			std::cout << "..." << std::endl;
+//			break;
+//		}
+//	}
+//
+//}
+
+bool Player::SurvivorsAtDoor()
+{
+	// Currently always returns false; update logic later if needed
+	bool survivorsAtDoor = false;
+	return survivorsAtDoor;
+}
+
+//bool Player::EncounterSurvivors() {
+//	bool EncounteredSurvivors = false;
+//	return EncounteredSurvivors;
+//}
