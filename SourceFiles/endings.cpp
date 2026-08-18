96970ec223843e10b06efde7c69fd1b76be6dceb
gaowenya110-svg <gaowenya110@gmail.com>
2026-08-18 11:25:16 +0800

    Update endings.cpp

--- SourceFiles/endings.cpp
+++ SourceFiles/endings.cpp
@@ -1,1 +1,18 @@
+#include "endings.h"
+#include <iostream>
 
+void endings::DeathEnding()
+{
+	std::cout << " The skinwalkers got to you... You died." << std::endl;
+
+}
+
+void endings::MilitaryEnding()
+{
+	std::cout << "The Military have come to rescue you. Congradulations, you win!" << std::endl;
+}
+
+void endings::helicopterEnding()
+{
+	std::cout << "You escape safely on a helicopter. Congradulations, you win!" << std::endl;
+}
