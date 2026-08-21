b8d63b0237c54f398b0456b6a5c47f2d4514023f
BrandonChai-91 <brandonchai91@gmail.com>
2026-08-21 09:49:47 +0800

    Update Player.h

--- HeaderFiles/Player.h
+++ HeaderFiles/Player.h
@@ -1,5 +1,6 @@
 #pragma once
-#include"Entity.h"
+#include "Entity.h"
+#include "Inventory.h"
 #include <string>
 
 class Player : public Entity
@@ -12,26 +13,36 @@ private:
     std::string Look4;
 
     std::string Dialogue1;
-    std::string Dialogue2;
+	std::string Dialogue2;
 	std::string Dialogue3;
 
+	Inventory GearInventory;
 
 public:
 	Player();
+	~Player() override;
+
+	bool AddEquipment(Equipment* equipment);
+	bool EquipItem(int inventoryIndex);
+	void ShowEquipment();
+	void EquipmentMenu();
+
+	int GetEquipmentCount();
+	Equipment* GetEquipment(int inventoryIndex);
+	Equipment* GetEquippedGear();
+	int GetTotalAttackPoints();
 
 	void Speak(int choice);
 
-	std::string GetLook0(); //happy
-	std::string GetLook1(); //angry
-	std::string GetLook2(); //crying
-	std::string GetLook3(); //shocked
-	std::string GetLook4(); //bored
+	std::string GetLook0(); // happy
+	std::string GetLook1(); // angry
+	std::string GetLook2(); // crying
+	std::string GetLook3(); // shocked
+	std::string GetLook4(); // bored
 
-
-
-	//skinwalker
 	bool SkinwalkerAtDoor();
 	bool EncounterSkinwalker();
+
 	//actions
 	void SkinWalkerAtDoor(int choice);
 	void EncounterSkinwalker(int choice);
@@ -39,9 +50,10 @@ public:
 	//survivors
 	bool SurvivorsAtDoor();
 	//bool EncounterSurvivors();
+
 	//actions
 	void SurvivorsAtDoor(int choice);
 	//void EncounterSurvivors(int choice);
 
-};
 
+};
