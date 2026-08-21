5d6e7c302b22feee2d0884026b53032719d8f9ed
BrandonChai-91 <brandonchai91@gmail.com>
2026-08-21 09:53:23 +0800

    Update World.h

--- HeaderFiles/World.h
+++ HeaderFiles/World.h
@@ -1,21 +1,68 @@
 #pragma once
-#include "Object.h"
+
+#include "Chunks.h"
+#include "Material.h"
+
+// All item and equipment types that can appear in the world.
+enum class ItemType
+{
+	Food,
+	Water,
+	Gun,
+	RealKnife
+};
+
+enum class EquipmentSlot
+{
+	None,
+	Gear
+};
+
+class Player;
+
+// World stores the day, turns, resources and exploration state.
 class World
 {
+private:
 	int Days;
-	int Place[5];
-	Object* Objects[10];
+	int Turns;
+	int CurrentChunk;
+	int PlayerX;
+	int PlayerY;
 
 	Material Water;
 	Material Food;
+	Chunks chunks[4];
+
+	void CreateObjects();
+	Object* GetNearbyObject();
 
 public:
 	World();
 
-	void SearchForSupplies();
+	int GetDays();
+	int GetTurns();
+	int GetCurrentChunk();
+	int GetPlayerX();
+	int GetPlayerY();
 
-	~World();
+	int GetFoodQuantity();
+	int GetWaterQuantity();
+	int GetMaximumFood();
+	int GetMaximumWater();
 
-	int getDays();
+	void UseTurn();
+	void EndTurns();
+	void NextDay();
+
+	bool UseDailySupplies(int peopleToFeed);
+
+	void Explore(Player* player);
+	bool MovePlayer(char direction, Player* player = nullptr);
+	bool CollectNearbySupply();
+	bool CollectNearbyItem(Player* player);
+	void ResetExplorationPosition();
+	void PrintLegend();
+
+	Chunks& GetChunk(int chunkNumber);
 };
-
