3cf21052206f829f145d8b78333e2314c0a94fc2
shan <shan.lyxs9@gmail.com>
2026-08-24 10:48:22 +0800

    Refactor Player class and add state variables
    Refactored Player class by consolidating string member variables and adding state variables for hunger and thirst. Updated method signatures and added new methods for item pickup and encounters.

--- HeaderFiles/Player.h
+++ HeaderFiles/Player.h
@@ -5,37 +5,66 @@
 class Player : public Entity
 {
 private:
-    std::string Look0;
-    std::string Look1;
-    std::string Look2;
-    std::string Look3;
-    std::string Look4;
+	std::string Look0, Look1, Look2, Look3, Look4;
 
-    std::string Dialogue1;
-    std::string Dialogue2;
-	std::string Dialogue3;
+	std::string Dialogue1, Dialogue2, Dialogue3;
 
+	//Player's states
+	int hunger;
+	int thirst;
+
+	//Encounter flags
+	bool skinwalkerEncounter;
+	bool survivorsEncounter;
+
+	int breadCount;
+	int waterCount;
+	int medicalKitCount;
 
 public:
 	Player();
+
+	void UpdateOverTime();
+	bool IsDead();
 
 	void Speak(int choice);
 
 	std::string GetLook0(); //happy
 	std::string GetLook1(); //angry
-	std::string GetLook2(); //crying
+	std::string GetLook2(); //bored
 	std::string GetLook3(); //shocked
-	std::string GetLook4(); //bored
+	std::string GetLook4(); //dead
 
+	//show in grid
+	char GetGridSymbol();
+	void DisplayStatus();
 
+	//Item pickup
+	void PickUpBread();
+	void PickUpWater();
+	void PickUpKnife();
+	void PickUpMedicalKit();
+
+	//bag actions
+	void ShowBag();
+	void ConsumeBread();
+	void ConsumeWater();
+	void ConsumeMedicalKit();
+	void DropBread();
+	void DropWater();
+	void DropMedicalKit();
+
+	//Encounters
+	void EncounterSkinwalker();
+	void EncounterSurvivors();
 
 	//skinwalker
 	bool SkinwalkerAtDoor();
-	bool EncounterSkinwalker();
+	//bool EncounterSkinwalker();
 	//actions
 	void SkinWalkerAtDoor(int choice);
 	void EncounterSkinwalker(int choice);
-	
+
 	//survivors
 	bool SurvivorsAtDoor();
 	//bool EncounterSurvivors();
@@ -43,5 +72,11 @@ public:
 	void SurvivorsAtDoor(int choice);
 	//void EncounterSurvivors(int choice);
 
-};
+	void BattleSkinwalker();
 
+	void SetHunger(int value);
+	void SetThirst(int value);
+	int GetHunger() const;
+	int GetThirst() const;
+
+};
